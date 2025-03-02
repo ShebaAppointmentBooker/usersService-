d7cab113a95fc436a6b2f085121a038f80520238
LiorAviv <lioraviv1312@gmail.com>
2025-03-02 21:27:05 +0200

    fixed env bug

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv'
-import doctorRoutes from './routes/doctorRoutes';
-import patientRoutes from './routes/patientRoutes';
-
+import * as dotenv from "dotenv";
 dotenv.config();
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import doctorRoutes from "./routes/doctorRoutes";
+import patientRoutes from "./routes/patientRoutes";
 
 const app = express();
 app.use(express.json());
@@ -13,9 +12,9 @@ app.use(express.json());
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
@@ -23,9 +22,9 @@ const connectDB = async () => {
 connectDB();
 
 // Use routes
-app.use('/users/doctors', doctorRoutes);
-app.use('/users/patients', patientRoutes);
+app.use("/users/doctors", doctorRoutes);
+app.use("/users/patients", patientRoutes);
 
 app.listen(5000, () => {
-  console.log('Server running on port 5000');
+  console.log("Server running on port 5000");
 });
